31c6647538a27150d589182ba9b8ea32a391daba
615940742@qq.com <whtlikegxy2004>
2020-07-12 16:30:00 +0800

    es6 async

--- src/views/Study/ES6/Async-Await.vue
+++ src/views/Study/ES6/Async-Await.vue
@@ -4,15 +4,48 @@
       <div class="panel-title">Async-Await</div>
     </div>
     <div class="panel-body panel-body-box">
-      <a-form :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-        <p>
-          <span>price:</span>
-          <span>{{ price }}</span>
-        </p>
-        <a-button type="primary" @click="getPrice" :loading="btnLoading">
-          获取
-        </a-button>
-      </a-form>
+      <div>
+        <blockquote>1.async 是 ES7 才有的与异步操作有关的关键字，和 Promise ， Generator 有很大关联的。</blockquote>
+      </div>
+      <div>
+        <blockquote>2.返回值:async 函数返回一个 Promise 对象，可以使用 then 方法添加回调函数。</blockquote>
+      </div>
+      <hr>
+      <div>
+        <blockquote>案例一：非异步 - return</blockquote>
+        <pre class="well">
+async helloAsync() {
+  return 'helloAsync'
+},
+        </pre>
+        <a-button-group>
+          <a-button type="primary" @click="handleClick1">点击查看结果，返回promise对象</a-button>
+          <a-button type="danger" @click="handleClick2">点击查看返回值</a-button>
+        </a-button-group>
+      </div>
+      <hr>
+      <div>
+        <blockquote>案例二：异步 - setTimeout</blockquote>
+        <pre class="well">
+testWait(num) {
+  return new Promise((resolve, reject) => {
+    // 模拟定义一个返回数据
+    num += 30
+    // 模拟3秒后才返回数据
+    setTimeout(() => {
+      resolve(num)
+    }, 3000)
+  })
+},
+        </pre>
+        <a-form :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+          <p>
+            <a-input v-model.number="price"></a-input>
+            <span>price:{{ price }}</span>
+          </p>
+          <a-button type="primary" @click="getPrice" :loading="btnLoading">点击等待3秒，获取结果</a-button>
+        </a-form>
+      </div>
     </div>
   </div>
 </template>
@@ -27,20 +60,34 @@ export default {
     }
   },
   methods: {
-    async getPrice() {
-      this.btnLoading = true
-      this.price = await this.asyncGetPrice()
-      this.btnLoading = false
+    // 非异步：return
+    async helloAsync() {
+      return 'helloAsync'
     },
-    asyncGetPrice() {
+    handleClick1() {
+      console.log(this.helloAsync()) // Promise {<resolved>: "helloAsync"}
+      this.$message.info(typeof this.helloAsync(), 3)
+    },
+    handleClick2() {
+      this.helloAsync().then(res => {
+        this.$message.info(res, 3)
+      })
+    },
+    // 异步：setTimeout
+    testWait(num) {
       return new Promise((resolve, reject) => {
         // 模拟定义一个返回数据
-        const num = 30
+        num += 30
         // 模拟3秒后才返回数据
         setTimeout(() => {
           resolve(num)
         }, 3000)
       })
+    },
+    async getPrice() {
+      this.btnLoading = true
+      this.price = await this.testWait(this.price)
+      this.btnLoading = false
     }
   }
 }
