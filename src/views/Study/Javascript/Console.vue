e4e88753c07d71526ec9c053b2a397b367152858
615940742@qq.com <whtlikegxy2004>
2020-07-12 18:27:02 +0800

    console

--- src/views/Study/Javascript/Console.vue
+++ src/views/Study/Javascript/Console.vue
@@ -1,34 +1,77 @@
 <template>
   <div class="panel panel-primary">
     <div class="panel-heading">
-      <div class="panel-title">Console</div>
+      <div class="panel-title">window.console</div>
     </div>
     <div class="panel-body panel-body-box">
-      <a-button-group>
-        <a-button type="primary" @click="log">console.log(obj)</a-button>
-        <a-button type="primary" @click="info">console.info(obj)</a-button>
-        <a-button type="primary" @click="dir">console.dir(obj)</a-button>
-        <a-button type="primary" @click="dirxml">console.dirxml(obj)</a-button>
+      <div>
+        <blockquote>console的所有方法</blockquote>
+        <ul class="list-unstyled">
+          <li v-for="(item, index) in consoleObj" :key="item">
+            <span class="color-blue">{{ index+1 }}: </span> <span class="color-red">{{ item }}</span>
+          </li>
+        </ul>
+      </div>
+      <hr>
+      <div>
+        <blockquote>1.console.debug</blockquote>
         <a-button type="primary" @click="debug">console.debug(obj)</a-button>
-        <a-button type="primary" @click="warn">console.warn(obj)</a-button>
+      </div>
+      <hr>
+      <div>
+        <blockquote>2.console.error</blockquote>
         <a-button type="primary" @click="error">console.error(obj)</a-button>
-        <a-button type="primary" @click="count">console.count(obj)</a-button>
-      </a-button-group>
-      <hr/>
-
-      <a-button-group>
-        <a-button type="primary" @click="assert">console.assert(obj)</a-button>
-      </a-button-group>
-      <hr/>
-
-      <a-button-group>
+      </div>
+      <hr>
+      <div>
+        <blockquote>3.console.info</blockquote>
+        <a-button type="primary" @click="info">console.info(obj)</a-button>
+      </div>
+      <hr>
+      <div>
+        <blockquote>4.console.log</blockquote>
+        <a-button type="primary" @click="log">console.log(obj)</a-button>
+      </div>
+      <hr>
+      <div>
+        <blockquote>5.console.warn</blockquote>
+        <a-button type="primary" @click="warn">console.warn(obj)</a-button>
+      </div>
+      <hr>
+      <div>
+        <blockquote>6.console.dir</blockquote>
+        <a-button type="primary" @click="dir">console.dir(obj)</a-button>
+      </div>
+      <hr>
+      <div>
+        <blockquote>7.console.dirxml</blockquote>
+        <a-button type="primary" @click="dirxml">console.dirxml(obj)</a-button>
+      </div>
+      <hr>
+      <div>
+        <blockquote>10~12.console.groupCollapsed</blockquote>
         <a-button type="primary" @click="groupCollapsed">console.groupCollapsed(obj)</a-button>
-      </a-button-group>
-      <hr/>
-
-      <a-button-group>
+      </div>
+      <hr>
+      <div>
+        <blockquote>13.console.clear</blockquote>
+        <a-button type="primary" @click="clear">console.clear()</a-button>
+      </div>
+      <hr>
+      <div>
+        <blockquote>14~15.console.count</blockquote>
+        <a-button type="primary" @click="count">console.count(obj)</a-button>
+      </div>
+      <hr>
+      <div>
+        <blockquote>16.console.assert</blockquote>
+        <a-button type="primary" @click="assert">console.assert(obj)</a-button>
+      </div>
+      <hr>
+      <div>
+        <blockquote>19~21.console.time</blockquote>
         <a-button type="primary" @click="time">console.time()</a-button>
-      </a-button-group>
+      </div>
     </div>
   </div>
 </template>
@@ -45,6 +88,12 @@ export default {
       }
     }
   },
+  computed: {
+    consoleObj() {
+      return Object.keys(console)
+    }
+  },
+  created() { },
   methods: {
     log() {
       console.log(this.obj)
@@ -77,6 +126,9 @@ export default {
       console.groupCollapsed()
       console.dir(this.obj)
       console.groupEnd()
+    },
+    clear() {
+      console.clear()
     },
     time() {
       const timeName = 'cost time is:'
