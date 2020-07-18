89b22a06483832506701b24add2acc9eb3f6e222
615940742@qq.com <whtlikegxy2004>
2020-07-18 22:44:46 +0800

    lodash debounce throttle

--- src/views/Study/Library/Lodash/Throttle.vue
+++ src/views/Study/Library/Lodash/Throttle.vue
@@ -7,7 +7,14 @@
       <div class="box">
         <a-row :gutter="30">
           <a-col :span="12">
-            <blockquote>input时，没有debounce</blockquote>
+            <blockquote>当鼠标不停时，没有throttle</blockquote>
+            <pre class="well">window.width = {{ this.quickInfo.w }}</pre>
+            <pre class="well">window.height = {{ this.quickInfo.h }}</pre>
+          </a-col>
+          <a-col :span="12">
+            <blockquote>当鼠标不停时，有throttle</blockquote>
+            <pre class="well">window.width = {{ this.slowInfo.w }}</pre>
+            <pre class="well">window.height = {{ this.slowInfo.h }}</pre>
           </a-col>
         </a-row>
       </div>
@@ -16,11 +23,42 @@
 </template>
 
 <script>
+import throttle from 'lodash.throttle'
+
 export default {
-  name: 'Throttle'
+  name: 'Throttle',
+  data() {
+    this.getInfoSlow = throttle(this.getInfoSlow, 1000)
+    return {
+      quickInfo: {
+        w: 0,
+        h: 0
+      },
+      slowInfo: {
+        w: 0,
+        h: 0
+      }
+    }
+  },
+  created() {
+    window.addEventListener('mousemove', e => {
+      this.getInfoQuick(e)
+    })
+    window.addEventListener('mousemove', e => {
+      this.getInfoSlow(e)
+    })
+  },
+  methods: {
+    getInfoQuick(e) {
+      this.quickInfo.w = e.clientX
+      this.quickInfo.h = e.clientY
+    },
+    getInfoSlow(e) {
+      this.slowInfo.w = e.clientX
+      this.slowInfo.h = e.clientY
+    }
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped lang="less"></style>
