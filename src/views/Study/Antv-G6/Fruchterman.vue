47dc96d9ceb69b112476e69c903489bc122aa44b
615940742@qq.com <whtlikegxy2004>
2020-07-19 22:37:34 +0800

    promise

--- src/views/Study/Antv-G6/Fruchterman.vue
+++ src/views/Study/Antv-G6/Fruchterman.vue
@@ -5,11 +5,11 @@
 </template>
 
 <script>
-import G6 from "@antv/g6";
-import data from "./data";
+import G6 from '@antv/g6'
+import data from './data'
 
 export default {
-  name: "Fruchterman",
+  name: 'Fruchterman',
   data() {
     return {
       data: data,
@@ -18,14 +18,14 @@ export default {
         nodeStateStyles: {
           highlightNode: {
             opacity: 1,
-            "text-shape": {
+            'text-shape': {
               // https://www.yuque.com/mo-college/weekly/ypovn2
               opacity: 1
             }
           },
           weakenNode: {
             opacity: 0.2,
-            "text-shape": {
+            'text-shape': {
               // https://www.yuque.com/mo-college/weekly/ypovn2
               opacity: 0.2
             }
@@ -33,146 +33,141 @@ export default {
         },
         edgeStateStyles: {
           highlightEdgeDark: {
-            stroke: "#ffffff"
+            stroke: '#ffffff'
           },
           weakenEdgeDark: {
-            stroke: "#071631"
+            stroke: '#071631'
             // stroke: 'red'
           },
           weakenEdgeLight: {
-            stroke: "#f5f5f5"
+            stroke: '#f5f5f5'
           }
         },
         defaultNode: {
-          color: "#5B8FF9",
+          color: '#5B8FF9',
           style: {
             // lineWidth: 2,
             // fill: '#C6E5FF'
           },
           labelCfg: {
             style: {
-              fill: "white"
+              fill: 'white'
             }
           }
         },
         defaultEdge: {
-          type: "quadratic",
+          type: 'quadratic',
           style: {
-            stroke: "#fa0f1b",
+            stroke: '#fa0f1b',
             lineWidth: 1,
             lineAppendWidth: 2,
             endArrow: {
               // path: constants.G6ArrowPath,
-              fill: "#e2e2e2"
+              fill: '#e2e2e2'
             }
           },
           labelCfg: {
             autoRotate: true,
             style: {
-              fill: "white"
+              fill: 'white'
             }
           }
         }
       }
-    };
+    }
   },
   created() {},
   mounted() {
-    this.initDiscovery();
+    this.initDiscovery()
   },
   methods: {
     async initDiscovery() {
       const edgeColor =
-        this.theme === "light" || this.theme === "blue" ? "#e2e2e2" : "#324E6B";
+        this.theme === 'light' || this.theme === 'blue' ? '#e2e2e2' : '#324E6B'
       const edgeLabelColor =
-        this.theme === "light" || this.theme === "blue" ? "#01172E" : "#F5F5F5";
+        this.theme === 'light' || this.theme === 'blue' ? '#01172E' : '#F5F5F5'
       //
-      this.defaultConf.defaultEdge.style.stroke = edgeColor;
-      this.defaultConf.defaultEdge.style.endArrow.fill = edgeColor;
-      this.defaultConf.defaultEdge.labelCfg.style.fill = edgeLabelColor;
-      this.defaultConf.defaultNode.labelCfg.style.fill = edgeLabelColor;
-
-      console.log(this.data);
-
+      this.defaultConf.defaultEdge.style.stroke = edgeColor
+      this.defaultConf.defaultEdge.style.endArrow.fill = edgeColor
+      this.defaultConf.defaultEdge.labelCfg.style.fill = edgeLabelColor
+      this.defaultConf.defaultNode.labelCfg.style.fill = edgeLabelColor
+      console.log(this.data)
       // const nodes = this.optimizeNodes(this.data.nodes)
       // const edges = this.optimizeEdges(this.data.edges)
       // const simplifyEdges = this.optimizeEdges(this.data.simplifyEdges)
-
-      const nodes = this.data.nodes;
-      const edges = this.data.edges;
-      const simplifyEdges = this.data.simplifyEdges;
-
+      const nodes = this.data.nodes
+      const edges = this.data.edges
+      const simplifyEdges = this.data.simplifyEdges
       // 拼接成作图的数据
       this.graphData = {
         nodes,
         edges,
         simplifyEdges
-      };
-
+      }
       // 主业务
       this.createChart1({ nodes, edges: simplifyEdges }).then(graph => {
-        setTimeout(() => {}, 1000);
-        setTimeout(() => {}, 3000);
+        setTimeout(() => {}, 1000)
+        setTimeout(() => {}, 3000)
         setTimeout(() => {
-          this.graphReady = true;
-          graph.fitView(20);
-        }, 4000);
+          this.graphReady = true
+          graph.fitView(20)
+        }, 4000)
         setTimeout(() => {
           // graph.updateLayout({
           //   gravity: 1
           // })
-        }, 5000);
-      });
+        }, 5000)
+      })
     },
 
     // 优化【节点】
     optimizeNodes(nodes) {
       return nodes.map(node => {
-        node.size = 70;
-        node.style = node.style || {};
+        node.size = 70
+        node.style = node.style || {}
 
         // 人员
-        if (node.type === "USER") {
-          node.type = "image";
-          node.size = 40;
-          if (!node.initScore) node.size = 40;
-          if (node.initScore > 0 && node.initScore < 200) node.size = 60;
-          if (node.initScore >= 200) node.size = 80;
+        if (node.type === 'USER') {
+          node.type = 'image'
+          node.size = 40
+          if (!node.initScore) node.size = 40
+          if (node.initScore > 0 && node.initScore < 200) node.size = 60
+          if (node.initScore >= 200) node.size = 80
           // node.img = `${this.imageHost}${node.headPortrait}?imgType=compressionSmall`
-          node.img = "./1.jpg";
-          node.label = node.name;
+          node.img = './1.jpg'
+          node.label = node.name
           node.clipCfg = {
             show: true,
-            type: "circle",
+            type: 'circle',
             r: node.size / 2,
             style: {
-              stroke: "#eaff8f",
-              shadowColor: "#000",
+              stroke: '#eaff8f',
+              shadowColor: '#000',
               shadowBlur: 10,
               lineWidth: 2
             }
-          };
+          }
         }
         // 任务
-        if (node.type === "TASK") {
-          node.size = 24;
-          if (!node.okrScore || node.okrScore <= 8) node.size = 24;
-          if (node.okrScore > 8 && node.okrScore <= 12) node.size = 32;
-          if (node.okrScore > 12 && node.okrScore <= 17) node.size = 40;
-          if (node.okrScore > 17) node.size = 48;
+        if (node.type === 'TASK') {
+          node.size = 24
+          if (!node.okrScore || node.okrScore <= 8) node.size = 24
+          if (node.okrScore > 8 && node.okrScore <= 12) node.size = 32
+          if (node.okrScore > 12 && node.okrScore <= 17) node.size = 40
+          if (node.okrScore > 17) node.size = 48
 
-          node.style.fill = "#dbdbdb";
+          node.style.fill = '#dbdbdb'
           // let taskIcon = taskIconGrey
-          if (node.taskType === "挑战任务") {
-            node.style.fill = "#FFF2B2";
+          if (node.taskType === '挑战任务') {
+            node.style.fill = '#FFF2B2'
             // taskIcon = taskIconYellow
           }
-          if (node.taskType === "专项任务") {
-            node.style.fill = "#B7E8CD";
+          if (node.taskType === '专项任务') {
+            node.style.fill = '#B7E8CD'
             // taskIcon = taskIconGreen
           }
-          if (node.taskType === "部门任务") {
-            node.style.fill = "#F1C1C7";
+          if (node.taskType === '部门任务') {
+            node.style.fill = '#F1C1C7'
             // taskIcon = taskIconRed
           }
           node.icon = {
@@ -180,65 +175,65 @@ export default {
             width: node.size - 10,
             height: node.size - 10
             // img: taskIcon
-          };
-          node.style.lineWidth = 0;
+          }
+          node.style.lineWidth = 0
         }
         // 部门
-        if (node.type === "DEPART") {
-          node.size = 60;
-          node.style.fill = "#29A8FF";
-          node.style.lineWidth = 0;
-          node.label = node.abbreviation || node.name;
+        if (node.type === 'DEPART') {
+          node.size = 60
+          node.style.fill = '#29A8FF'
+          node.style.lineWidth = 0
+          node.label = node.abbreviation || node.name
         }
-        return node;
-      });
+        return node
+      })
     },
     // 优化【边线】
     optimizeEdges(edges) {
       const multiSameDirectionEdges = edges.reduce((result, each, index) => {
-        const key = each.source + each.target;
-        result[key] ? result[key].push(index) : (result[key] = [index]);
-        return result;
-      }, {});
-      console.log("multiSameDirectionEdges:", multiSameDirectionEdges);
+        const key = each.source + each.target
+        result[key] ? result[key].push(index) : (result[key] = [index])
+        return result
+      }, {})
+      console.log('multiSameDirectionEdges:', multiSameDirectionEdges)
       return edges.map((edge, index) => {
-        edge.id = "edge|" + index;
+        edge.id = 'edge|' + index
         // edge.label = edge.relationshipAbbreviation
-        const key = edge.source + edge.target;
-        const sameEdgeCounts = multiSameDirectionEdges[key].length;
-        edge.curveOffset = -30;
+        const key = edge.source + edge.target
+        const sameEdgeCounts = multiSameDirectionEdges[key].length
+        edge.curveOffset = -30
         if (sameEdgeCounts > 1) {
           const theIndex = multiSameDirectionEdges[key].findIndex(
             e => e === index
-          );
-          edge.curveOffset = 40 * theIndex + 30;
+          )
+          edge.curveOffset = 40 * theIndex + 30
         }
         edge.style =
           edge.style ||
-          JSON.parse(JSON.stringify(this.defaultConf.defaultEdge.style));
+          JSON.parse(JSON.stringify(this.defaultConf.defaultEdge.style))
         edge.style.startArrow = edge.twoWay
           ? this.defaultConf.defaultEdge.style.endArrow
-          : false;
-        return edge;
-      });
+          : false
+        return edge
+      })
     },
 
     createChart1(data) {
       return new Promise(resolve => {
         // if (this.graph[this.graphType].graph) return
         // 初始化
-        const ref = this.$refs["canvas"];
-        console.log(ref.clientWidth);
+        const ref = this.$refs['canvas']
+        console.log(ref.clientWidth)
         const graph = new G6.Graph({
           container: ref,
           width: ref.clientWidth,
           height: ref.clientHeight,
           modes: {
-            default: ["drag-canvas", "drag-node", "zoom-canvas"]
+            default: ['drag-canvas', 'drag-node', 'zoom-canvas']
           },
           layout: {
             // workerEnabled: true,
-            type: "fruchterman",
+            type: 'fruchterman',
             gravity: 0.5,
             speed: 5
             // linkDistance: 100,
@@ -250,10 +245,10 @@ export default {
             duration: 1000 // Number，一次动画的时长
           },
           ...this.defaultConf
-        });
+        })
 
         // 设置数据
-        graph.data(this.data);
+        graph.data(this.data)
         // 设置事件
         // graph.on('node:mouseenter', evt => {
         //     this.onMouseenterNode(graph, evt)
@@ -283,14 +278,14 @@ export default {
         // })
 
         // 渲染
-        graph.render();
+        graph.render()
         // graph.changeData(data || {})
         // this.graph[this.graphType].graph = graph
-        resolve(graph);
-      });
+        resolve(graph)
+      })
     }
   }
-};
+}
 </script>
 
 <style lang="less" scoped>
--- src/views/Study/ES6/Promise.vue
+++ src/views/Study/ES6/Promise.vue
@@ -4,7 +4,106 @@
       <div class="panel-title">Promise</div>
     </div>
     <div class="panel-body panel-body-box">
-      123
+      <div>
+        <!-- resolve -->
+        <div class="box">
+          <a-row :gutter="24">
+            <a-col :span="24">
+              <blockquote>1.Promise.resolve()</blockquote>
+            </a-col>
+            <a-col :span="12">
+            <pre class="well">
+const promise = Promise.resolve('aaa')
+
+promise.then(res => {
+  console.log(res) // aaa
+})
+            </pre>
+            </a-col>
+            <a-col :span="12">
+            <pre class="well">
+const promise = new Promise(resolve => {
+  resolve('aaa')
+})
+
+promise.then(res => {
+  console.log(res) // aaa
+})
+            </pre>
+            </a-col>
+          </a-row>
+        </div>
+        <!-- reject -->
+        <div class="box">
+          <a-row :gutter="24">
+            <a-col :span="24">
+              <blockquote>2.Promise.reject()</blockquote>
+            </a-col>
+            <a-col :span="12">
+            <pre class="well">
+const promise = Promise.reject('aaa')
+
+promise.then(res => {
+  // ...
+}).catch(err => {
+  console.log(err) // aaa
+})
+            </pre>
+            </a-col>
+            <a-col :span="12">
+            <pre class="well">
+const promise = new Promise((resolve, reject) => {
+  reject('aaa')
+})
+
+promise.then(res => {
+  // ...
+}).catch(err => {
+  console.log(err) // aaa
+})
+            </pre>
+            </a-col>
+          </a-row>
+        </div>
+        <!-- all -->
+        <div class="box">
+          <a-row :gutter="24">
+            <a-col :span="24">
+              <blockquote>3.Promise.all()</blockquote>
+            </a-col>
+            <a-col :span="12">
+            <pre class="well">
+const promise_1 = Promise.resolve('aaa')
+const promise_2 = Promise.resolve('bbb')
+const promise_3 = Promise.resolve('ccc')
+
+promise.all([promise_1, promise_2, promise_3]).then(res => {
+  console.log(res) // ['aaa', 'bbb', 'ccc']，类型：数组；用结构取值
+}
+            </pre>
+            </a-col>
+          </a-row>
+        </div>
+        <!-- race -->
+        <div class="box">
+          <a-row :gutter="24">
+            <a-col :span="24">
+              <blockquote>4.Promise.race()</blockquote>
+            </a-col>
+            <a-col :span="12">
+            <pre class="well">
+const promise_1 = Promise.resolve('aaa')
+const promise_2 = Promise.reject('bbb')
+const promise_3 = Promise.resolve('ccc')
+
+promise.all([promise_1, promise_2, promise_3]).then(res => {
+  console.log(res) // 'aaa'， 第二个报错，后面不执行
+}
+            </pre>
+            </a-col>
+          </a-row>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,6 +114,12 @@ export default {
   data() {
     return {
       debug: true
+    }
+  },
+  computed: {
+    promiseResolve() {
+      // eslint-disable-next-line vue/no-async-in-computed-properties
+      return Promise.resolve('这是一个resolve结果')
     }
   },
   created() {
@@ -38,6 +143,4 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
