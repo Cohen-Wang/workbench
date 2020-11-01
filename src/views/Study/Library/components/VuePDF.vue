aeeaec22c745cfff3cc0e2c69ac0e676c903eb84
615940742@qq.com <whtlikegxy2004>
2020-11-01 22:52:31 +0800

    【插件】 - vue-pdf

--- src/views/Study/Library/components/VuePDF.vue
+++ src/views/Study/Library/components/VuePDF.vue
@@ -6,9 +6,26 @@
       </div>
       <div slot="component-body">
         <!-- ... -->
-        <pdf ref="pdf"
-             :src="pdfUrl">
-        </pdf>
+        <div>
+          <div class="content">
+            <div class="content-header">
+              <a-button type="primary" size="small" @click="changePdfPage(0)">上一页</a-button>
+              <a-button type="primary" size="small" @click="changePdfPage(1)">下一页</a-button>
+              <span style="color: #FFFFFF;"><i>{{ pdf.currentPage }}</i> / <i>{{ pdf.pageCount }}</i></span>
+            </div>
+            <div class="content-body">
+              <div class="content-body-center">
+                <pdf ref="pdf"
+                     :src="pdf.src"
+                     :page="pdf.currentPage"
+                     @num-pages="pdf.pageCount = $event"
+                     @page-loaded="pdf.currentPage = $event"
+                     @loaded="loadPdfHandler"><!--加载事件-->
+                </pdf>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </my-component>
   </div>
@@ -29,8 +46,17 @@ export default {
   data() {
     return {
       visible: false,
-      pdfUrl: 'https://dakaname.oss-cn-hangzhou.aliyuncs.com/file/2018-12-28/1546003237411.pdf'
+      pdf: {
+        src: 'https://dakaname.oss-cn-hangzhou.aliyuncs.com/file/2018-12-28/1546003237411.pdf', // pdf文件地址
+        currentPage: 1, // pdf文件页码
+        pageCount: 0, // pdf文件总页数
+        fileType: 'pdf' // 文件类型
+      }
     }
+  },
+  created() {
+    // 有时PDF文件地址会出现跨域的情况,这里最好处理一下
+    this.pdf.src = pdf.createLoadingTask(this.pdf.src)
   },
   methods: {
     // +---------------------------------------------------------------------------------------------
@@ -41,13 +67,50 @@ export default {
     },
     goBack() {
       this.visible = false
-    }
+    },
     // +---------------------------------------------------------------------------------------------
     // | 正文
     // +---------------------------------------------------------------------------------------------
+    // 改变PDF页码,val传过来区分上一页下一页的值,0上一页,1下一页
+    changePdfPage(val) {
+      // console.log(val)
+      if (val === 0 && this.pdf.currentPage > 1) {
+        this.pdf.currentPage--
+        // console.log(this.currentPage)
+      }
+      if (val === 1 && this.pdf.currentPage < this.pdf.pageCount) {
+        this.pdf.currentPage++
+        // console.log(this.currentPage)
+      }
+    },
+    // pdf加载时
+    loadPdfHandler(e) {
+      this.$message.info('pdf资源加载成功')
+      this.pdf.currentPage = 1 // 加载的时候先加载第一页
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+  .content {
+    background-color: #333333;
+    height: calc(100vh - 187px);
+    overflow-y: auto;
+
+    .content-header {
+      padding: 6px 10px;
+      background-color: rgba(0, 0, 0, 0.65);
+    }
+
+    .content-body {
+      padding-top: 15px;
+      padding-bottom: 15px;
+
+      .content-body-center {
+        width: 1000px;
+        margin: 0 auto;
+      }
+    }
+  }
 </style>
