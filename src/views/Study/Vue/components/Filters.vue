4448f766514dbd86ef024fe83fa3c93c4a1b36d1
cohen_wang <615940742@qq.com>
2020-11-16 20:36:24 +0800

    【vue】 - filters

---
+++ src/views/Study/Vue/components/Filters.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-show="visible" class="root">
+    <my-component>
+      <div slot="component-heading">
+        <go-back @goBack="goBack"/>
+      </div>
+      <div slot="component-body">
+        <!-- ... -->
+        <h1>filter</h1>
+      </div>
+    </my-component>
+  </div>
+</template>
+
+<script>
+import MyComponent from '@/components/MyComponent'
+import GoBack from '@/components/GoBack'
+
+export default {
+  name: 'Filter',
+  components: {
+    GoBack,
+    MyComponent
+  },
+  data() {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    // +---------------------------------------------------------------------------------------------
+    // | 页面
+    // +---------------------------------------------------------------------------------------------
+    show() {
+      this.visible = true
+    },
+    goBack() {
+      this.visible = false
+    }
+    // +---------------------------------------------------------------------------------------------
+    // | 正文
+    // +---------------------------------------------------------------------------------------------
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
