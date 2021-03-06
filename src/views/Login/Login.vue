e56a51f6a7f1385bad23adaf7edc117d75041f6f
615940742@qq.com <whtlikegxy2004>
2021-03-06 21:57:02 +0800

    logiin 优化

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -14,12 +14,12 @@
             账户密码登录
           </span>
           <a-form-model ref="loginForm"
-                        :model="loginForm"
-                        :rules="loginRules">
+                        :model="form"
+                        :rules="rules">
             <a-form-model-item prop="username">
               <a-input type="text"
                        allowClear
-                       v-model="loginForm.username"
+                       v-model="form.username"
                        placeholder="用户名">
                 <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)"/>
               </a-input>
@@ -27,21 +27,21 @@
             <a-form-model-item prop="password">
               <a-input type="password"
                        allowClear
-                       v-model="loginForm.password"
+                       v-model="form.password"
                        placeholder="密码">
                 <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)"/>
               </a-input>
             </a-form-model-item>
             <a-form-model-item>
-              <a-checkbox v-model="loginForm.autoLogin"
-                          :checked="loginForm.autoLogin">
+              <a-checkbox v-model="form.autoLogin"
+                          :checked="form.autoLogin">
                 记住我
               </a-checkbox>
               <a class="login-form-forgot" href="" style="float: right">忘记密码？</a>
               <a-button type="primary"
                         block
-                        :loading="loginLoading"
-                        @click.native.prevent="handleSubmit">
+                        :loading="loading"
+                        @click.native.prevent="onLogin">
                 登 录
               </a-button>
               <a href="javascript:void(0)" @click="$router.push('/register')">注册</a>
@@ -63,55 +63,57 @@
 <script>
 import Layout from './components/Layout'
 
+const FORM = {
+  username: 'admin',
+  password: '123456',
+  autoLogin: false
+}
+
+const RULES = {
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' }
+  ]
+}
+
 export default {
   name: 'Login',
   components: { Layout },
   data() {
     return {
-      loginForm: {
-        username: 'admin',
-        password: '123456',
-        autoLogin: false
-      },
-      loginRules: {
-        username: [
-          { required: true, message: '请输入用户名', trigger: 'blur' }
-        ],
-        password: [
-          { required: true, message: '请输入密码', trigger: 'blur' }
-        ]
-      },
-      loginLoading: false
+      form: JSON.parse(JSON.stringify(FORM)),
+      rules: Object.assign({}, RULES),
+      loading: false
     }
   },
   methods: {
     // 登录
-    handleSubmit() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loginLoading = true
-          this.$axios.post('http://jsonplaceholder.typicode.com/posts/1/comments').then(res => {
-            if (this.loginForm.username === 'admin' && this.loginForm.password === '123456') {
-              // 存储token
-              const token = JSON.stringify({ username: 'admini', password: '123456' })
-              localStorage.setItem('token', token)
-              // 跳转
-              this.$router.push('/')
-            } else {
-              this.$message.error('用户名或密码错误', 3)
-            }
-          }).finally(() => {
-            this.loginLoading = false
-          })
+    onLogin() {
+      this.$refs['loginForm'].validate(valid => {
+        if (!valid) return
+        this.login()
+      })
+    },
+    login() {
+      this.loading = true
+      this.$axios.post('http://jsonplaceholder.typicode.com/posts/1/comments').then(res => {
+        if (this.form.username === 'admin' && this.form.password === '123456') {
+          // 存储token
+          const token = JSON.stringify({ username: 'admin', password: '123456' })
+          localStorage.setItem('token', token)
+          // 跳转
+          this.$router.push('/')
         } else {
-          return false
+          this.$message.error('用户名或密码错误', 3)
         }
+      }).finally(() => {
+        this.loading = false
       })
     }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style lang="less" scoped></style>
