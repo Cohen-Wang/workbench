a42403b8dc9dca074b80a84f00a5cbbaf9668a5d
DESKTOP-J3VMULH\cohen <615940742@qq.com>
2021-03-08 21:43:15 +0800

    密码强度

--- src/utils/dist/form.util.js
+++ src/utils/dist/form.util.js
@@ -11,12 +11,23 @@
 export const isPasswordStrong = (rule, value, callback) => {
   // 如果没有内容，继续执行
   if (!value) return callback()
-  // 如果有内容，则判断
-  const reg = /^(?=.*[a-zA-Z])(?=.*\d).{8,20}$/
-  const isPasswordStrong = reg.test(value)
-  // 逻辑
-  if (!isPasswordStrong) return callback(new Error('密码弱，请设置8-20位字母加数字密码！'))
-  callback()
+  const debug = 2
+  if (debug === 1) {
+    // 如果有内容，则判断
+    const reg = /^(?=.*[a-zA-Z])(?=.*\d).{8,20}$/
+    const isPasswordStrong = reg.test(value)
+    // 逻辑
+    if (!isPasswordStrong) return callback(new Error('密码弱，请设置8-20位字母加数字密码！'))
+    callback()
+  }
+  if (debug === 2) {
+    // 如果有内容，则判断
+    const reg = /^.*(?=.{8,20})(?=.*\d)(?=.*[A-Z]{1,})(?=.*[a-z]{1,})(?=.*[!@#$%^&*?\(\)]).*$/
+    const isPasswordStrong = reg.test(value)
+    // 逻辑
+    if (!isPasswordStrong) return callback(new Error('密码最短8位，必须包含数字，小写字母，大写字母和特殊字符'))
+    callback()
+  }
 }
 
 /**
