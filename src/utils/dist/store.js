adcdf952e4fd7b76af309710cbfcfd765302623f
615940742@qq.com <whtlikegxy2004>
2021-03-13 15:58:52 +0800

    封装 store.js

--- src/utils/dist/store.js
+++ src/utils/dist/store.js
@@ -1,43 +1,79 @@
-
 /**
- * 存储sessionStorage
+ * sessionStorage
  */
-export const setStore = (params) => {
-  const { name, content, type } = params
-  const obj = {
-    dataType: typeof (content),
-    content: content,
-    type: type,
-    datetime: new Date().getTime()
+export const session = {
+  // 存储
+  set: params => {
+    const { name, content, type } = params
+    const obj = {
+      dataType: typeof (content),
+      content: content,
+      type: type,
+      datetime: new Date().getTime()
+    }
+    // 直接放到sessionStorage
+    window.sessionStorage.setItem(name, JSON.stringify(obj))
+  },
+  // 获取
+  get: params => {
+    const { name } = params
+    let obj = {}
+    let content
+    obj = window.sessionStorage.getItem(name)
+    if (!obj) return
+    obj = JSON.parse(obj)
+    if (obj.dataType === 'string') {
+      content = obj.content
+    } else if (obj.dataType === 'number') {
+      content = Number(obj.content)
+    } else if (obj.dataType === 'object') {
+      content = obj.content
+    }
+    return content
+  },
+  // 删除
+  remove: params => {
+    const { name } = params
+    window.sessionStorage.removeItem(name)
   }
-  // 直接放到sessionStorage
-  window.sessionStorage.setItem(name, JSON.stringify(obj))
 }
 
 /**
- * 获取sessionStorage
+ * 本地存储
  */
-export const getStore = (params) => {
-  const { name } = params
-  let obj = {}
-  let content
-  obj = window.sessionStorage.getItem(name)
-  if (obj) return
-  obj = JSON.parse(obj)
-  if (obj.dataType === 'string') {
-    content = obj.content
-  } else if (obj.dataType === 'number') {
-    content = Number(obj.content)
-  } else if (obj.dataType === 'object') {
-    content = obj.content
+export const local = {
+  // 存储
+  set: params => {
+    const { name, content, type } = params
+    const obj = {
+      dataType: typeof (content),
+      content: content,
+      type: type,
+      datetime: new Date().getTime()
+    }
+    // localStorage
+    window.localStorage.setItem(name, JSON.stringify(obj))
+  },
+  // 获取
+  get: params => {
+    const { name } = params
+    let obj = {}
+    let content
+    obj = window.localStorage.getItem(name)
+    if (!obj) return
+    obj = JSON.parse(obj)
+    if (obj.dataType === 'string') {
+      content = obj.content
+    } else if (obj.dataType === 'number') {
+      content = Number(obj.content)
+    } else if (obj.dataType === 'object') {
+      content = obj.content
+    }
+    return content
+  },
+  // 删除
+  remove: params => {
+    const { name } = params
+    window.localStorage.removeItem(name)
   }
-  return content
-}
-
-/**
- * 删除sessionStorage
- */
-export const removeStore = params => {
-  const { name } = params
-  window.sessionStorage.removeItem(name)
 }
